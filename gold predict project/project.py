b6604d2826c662191a465c7233647cff04115ff7
Ahmed Ehab <146896217+Ahmed-Ehab33@users.noreply.github.com>
2024-04-14 06:52:44 +0200

    Add files via upload

---
+++ gold predict project/project.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+
+
+data = pd.read_csv(".\gld_price_data.csv")
+x = data.iloc [:,[1,3,4,5]].to_numpy()
+y = data.iloc [:,[2]].to_numpy()
+
+gold = LinearRegression()
+gold.fit(x,y)
+(x[0])
+(y[0])
+
+
+print(gold.predict([[ 5086.66 , 73.56, 21.72, 1.09]]))
+
+
+
+plt.hist(data['GLD' ] ,  color='#FFA07A', edgecolor= 'black' )
+
+plt.xlabel('X-axis Label')
+plt.ylabel('Y-axis Label')
+plt.title('gold price predeiction')
+plt.show()
+#color='#FFA07A',,   edgecolor= 'black'
